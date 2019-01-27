2af5402c77abe140615ce9b8f5be1ae84906f3e5
Bens Hilaire <benshilaire@gmail.com>
2019-01-27 02:55:55 +0000

    Automatically backed up by Learn

--- arrays.js
+++ arrays.js
@@ -53,7 +53,7 @@ function destructivelyRemoveElementFromEndOfArrayy(array) {
 
 function addElementToBeginningOfArray(array, element) {
   element.unshift()
-  return array;
+  return element;
 }
 
   
