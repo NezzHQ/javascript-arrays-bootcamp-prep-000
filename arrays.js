1a807932e5f76728eecefd0956bed00820981633
Bens Hilaire <benshilaire@gmail.com>
2019-01-27 01:26:50 +0000

    Automatically backed up by Learn

--- arrays.js
+++ arrays.js
@@ -12,7 +12,7 @@ function accessElementInArray(array, index){
   return array[index];
 }
 
-function destructiveRemoveElementFromBeginningOfArray(array) {
+function destructivelyRemoveElementFromBeginningOfArray(array) {
   array.shift()
   return array 
 }
