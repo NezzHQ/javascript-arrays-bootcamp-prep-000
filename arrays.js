c5a787d94add15adf5dcde54669d9148fe295114
Bens Hilaire <benshilaire@gmail.com>
2019-01-27 00:17:53 +0000

    Automatically backed up by Learn

--- arrays.js
+++ arrays.js
@@ -1,8 +1,2 @@
 var chocolateBars = ['snickers', 'hundred grand', 'kitkat', 'skittles'];
 
-var superheroines = ['catwoman', 'she-hulk', 'mystique'];
-superheroines.push('wonder woman');
-
-var cities = ['New York', 'San Francisco',];
-cities.unshift('Philadelphia');
-
