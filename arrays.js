c32f61cb47a825ff110edc936860f78f9b771ca7
Bens Hilaire <benshilaire@gmail.com>
2019-01-27 02:33:26 +0000

    Automatically backed up by Learn

--- arrays.js
+++ arrays.js
@@ -45,6 +45,6 @@ function destructivelyAddElementToEndOfArray(array, element) {
 }
 
 function destructivelyRemoveElementFromEndOfArray(array) {
-  return array.slice(1)
+  return array.slice(0)
 }
 
