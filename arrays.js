8039ea42c30dc1d51ff9925daada151e966e9ef3
Bens Hilaire <benshilaire@gmail.com>
2019-01-27 00:23:15 +0000

    Automatically backed up by Learn

--- arrays.js
+++ arrays.js
@@ -1,5 +1,5 @@
 var chocolateBars = ['snickers', 'hundred grand', 'kitkat', 'skittles'];
 
 var addElementToBeginningOfArray(array,element) {
-  return addElementToBeginningOfArray [element,...array]
+  return addElementToBeginningOfArray [element, array];
 }
