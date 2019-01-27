f643bff2e5c0af83456e3efcc514c3714b38fccd
Bens Hilaire <benshilaire@gmail.com>
2019-01-27 01:40:19 +0000

    Automatically backed up by Learn

--- arrays.js
+++ arrays.js
@@ -17,6 +17,10 @@ function destructivelyRemoveElementFromBeginningOfArray(array) {
   return array 
 }
 
+function addElementToEndOfArray(array, element){
+  return [...array, element]
+} 
+// might have to delete code above
 
 function removeElementFromBeginningOfArray(array){
   return array.slice(1)
